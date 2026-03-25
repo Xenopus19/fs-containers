a1a926c2acc29b5e1bc37c4538c93d848b71a7d2
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2026-03-25 13:10:44 +0200

    workflow reset

--- todo-app/mongo/mongo-init.js/mongo-init.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB('the_database');
-
-db.createUser({
-  user: 'root', 
-  pwd: 'the_password',
-  roles: [{ role: 'readWrite', db: 'the_database' }],
-});
-
-db.createCollection('test_collection'); 
