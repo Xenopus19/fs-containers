02a19676eaad077506524add394276f2f2039a10
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2026-03-25 15:44:29 +0200

    Update apiClient.js

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:8080/api/`,
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080/api/',
 })
 
 export default apiClient
